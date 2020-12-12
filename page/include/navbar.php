28fdc98e21fb54123567c19dd7934b45b3b0ed0a
Garwalle <martin.jeanne@sts-sio-caen.info>
2020-12-12 14:35:54 +0100

    logout seems to not work, todo

--- page/include/navbar.php
+++ page/include/navbar.php
@@ -8,13 +8,17 @@
     <div class="right menu">
         <?php
         session_start();
-        $notCU = "<div class=\"item\"><div id=\"signup\" class=\"ui primary button\">Signup</div></div><div class=\"item\"><div id=\"login\" class=\"ui button\">Login</div></div>";
+        $notCU = 
+        "<div class=\"item\"><div id=\"signup\" class=\"ui primary button\">Signup</div></div>
+        <div class=\"item\"><div id=\"login\" class=\"ui button\">Login</div></div>";
         if (isset($_SESSION["CU_id"])) {
-            include(dirname(__DIR__) . '/../services/PDO_connection.php');
+            include('../services/PDO_connection.php');
             $sql = "SELECT id FROM user WHERE id = " . $_SESSION["CU_id"] . ";";
             $req = $bdd->query($sql);
             if ($req->rowCount() > 0) {
-                $CU = "<div class=\"item\"><div id=\"CU\">" . $_SESSION["CU_email"] . "</div></div><div class=\"item\"><div id=\"logout\" class=\"ui button\">Logout</div></div>";
+                $CU = 
+                "<div class=\"item\"><div id=\"CU\">" . $_SESSION["CU_email"] . "</div></div>
+                <div class=\"item\"><div onclick=\"location.href='../services/logout.php';\" class=\"ui button\">Logout</div></div>";
                 echo $CU;
             } else {
                 unset($_SESSION["CU_id"]);
---
+++ services/logout.php
@@ -0,0 +1,8 @@
+<?php
+// On supprime les variables de session
+unset($_SESSION["CU_id"]);
+unset($_SESSION["CU_username"]);
+unset($_SESSION["CU_email"]);
+unset($_SESSION["CU_userRole"]);
+
+header("Location: ../page/index.php");
