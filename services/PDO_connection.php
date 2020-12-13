6d98a238b031afb48844dd1cdd2e451bcae5bde5
Garwalle <martin.jeanne@sts-sio-caen.info>
2020-12-13 01:44:54 +0100

    change for security reasons

--- services/PDO_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connection à la base de donnée
-try {
-	$bdd = new PDO("mysql:host=localhost;dbname=chinook;charset=utf8", "root", "");
-} catch (Exception $e) {
-	$bdd->rollback();
-	throw $e;
-}
-?>
